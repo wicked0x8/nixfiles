0c3ac92a9d60f4bea8fea433f3cf2a29d19a0e4a
dubbber <scorpionolegovich@gmail.com>
2025-11-03 20:51:24 +0500

    finally installed dms, congrats

--- modules/nixos/desktop/addons/ly/default.nix
+++ modules/nixos/desktop/addons/ly/default.nix
@@ -25,9 +25,9 @@ in
 	animation = "colormix";
 	clock = "%c";
 
-	colormix_col1 = "0x${config.colorScheme.palette.base0F}";
-	colormix_col2 = "0x${config.colorScheme.palette.base05}";
-	colormix_col3 = "0x${config.colorScheme.palette.base03}";
+	#colormix_col1 = "0x${config.colorScheme.palette.base0F}";
+	#colormix_col2 = "0x${config.colorScheme.palette.base05}";
+	#colormix_col3 = "0x${config.colorScheme.palette.base03}";
       };
     };
     # services.displayManager.sddm.theme = "Elegant";
