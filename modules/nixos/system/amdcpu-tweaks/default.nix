72f8bec8dd581d62058e595987bba0bad48d853c
dubbber <scorpionolegovich@gmail.com>
2025-11-03 22:28:45 +0500

    finally installed dms, congrats

--- modules/nixos/system/amdcpu-tweaks/default.nix
+++ modules/nixos/system/amdcpu-tweaks/default.nix
@@ -11,8 +11,27 @@ in
 
   config = mkIf cfg.enable {
     boot.kernelParams = [ "amd_pstate=active" ];
-    powerManagement.enable = true;
-    powerManagement.cpuFreqGovernor = "schedutil";
+    powerManagement.powertop.enable = true; # enable powertop auto tuning on startup.
+
+    services.system76-scheduler.settings.cfsProfiles.enable = true; # Better scheduling for CPU cycles - thanks System76!!!
+    services.power-profiles-daemon.enable = false; # Disable GNOMEs power management
+    services.tlp = {
+      enable = true; # Enable TLP (better than gnomes internal power manager)
+      settings = {
+        CPU_BOOST_ON_AC = 1;
+        CPU_BOOST_ON_BAT = 1;
+        CPU_HWP_DYN_BOOST_ON_AC = 1;
+        CPU_HWP_DYN_BOOST_ON_BAT = 1;
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+        PLATFORM_PROFILE_ON_AC = "performance";
+        PLATFORM_PROFILE_ON_BAT = "balanced";
+        START_CHARGE_THRESH_BAT0 = 75;
+        STOP_CHARGE_THRESH_BAT0 = 81;
+      };
+    };
   };
 }
 
