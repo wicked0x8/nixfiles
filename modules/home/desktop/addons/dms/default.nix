58e3f8408be07c542276e7b9343883bc711405f7
dubbber <scorpionolegovich@gmail.com>
2025-11-03 12:57:48 +0500

    added mango, adding dms now

--- modules/home/desktop/addons/dms/default.nix
+++ modules/home/desktop/addons/dms/default.nix
@@ -3,6 +3,9 @@
 let
   cfg = config.mine.desktop.dms;
 in {
+
+  imports = [ inputs.dankMaterialShell.homeModules.dankMaterialShell.default ];
+
   options.mine.desktop.dms = {
     enable = lib.mkEnableOption "install dms";
   };
