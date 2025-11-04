51a4f6bda0d59b487c3772b0d9085c8d2fe67833
dubbber <scorpionolegovich@gmail.com>
2025-11-04 14:48:16 +0500

    fixed some matugen issues, installing prism

--- modules/nixos/desktop/mango/default.nix
+++ modules/nixos/desktop/mango/default.nix
@@ -18,6 +18,20 @@ in
 
   config = mkIf cfg.enable {
     programs.mango.enable = true;
+
+    # additional packages
+    services = {
+      polkit.enable = true;
+      upower.enable = true;
+    };
+    environment.systemPackages = with pkgs; [
+      polkit
+      xfce-polkit
+      xdg-desktop-portal,
+      xdg-desktop-portal-wlr
+      xdg-desktop-portal-gtk
+      upower
+    ];
   };
 }
 
