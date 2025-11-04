988418c9f2d216358a78d65ca742e59b652605c3
dubbber <scorpionolegovich@gmail.com>
2025-11-04 17:39:40 +0500

    gtk & qt theming

--- modules/nixos/desktop/mango/default.nix
+++ modules/nixos/desktop/mango/default.nix
@@ -23,12 +23,15 @@ in
     services = {
       upower.enable = true;
     };
-    security.polkit.enable = true;
     environment.systemPackages = with pkgs; [
       xdg-desktop-portal
       xdg-desktop-portal-wlr
       xdg-desktop-portal-gtk
       upower
+      qtmultimedia # todo: refactor this like it's not belonging here
+      libsForQt5.qtsvg
+      libsForQt6.qtsvg
+      adw-gtk3
     ];
   };
 }
