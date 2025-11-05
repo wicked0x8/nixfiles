77027d28db622f2fec78f81cff5b1fea6373e415
dubbber <scorpionolegovich@gmail.com>
2025-11-05 20:26:33 +0500

    added cliphist, kdepolkitagent, and env vars

--- modules/nixos/services/kde-polkit-agent/default.nix
+++ modules/nixos/services/kde-polkit-agent/default.nix
@@ -6,21 +6,24 @@ let
 in
 {
   options.mine.services.kde-polkit-agent = {
-    enable = mkEnableOption "enable kde-polkit-agent service";
+    enable = mkEnableOption "enable kde-polkit-agent user service";
   };
 
   config = mkIf cfg.enable {
-    systemd.user.services = {
-      "polkit-kde-autostart" = {
-        Unit = { Description = "autostart kde polkit agent"; };
-	Install = { WantedBy = [ "default.target" ]; };
-	Service = {
-	  ExecStart = pkgs.writeShellScript "kde-polkit-agent-start" ''
-	    ${pkgs.kdePackages.polkit-kde-agent-1}/libexec/polkit-kde-authentication-agent-1
-	  '';
-	};
+    systemd.user.services.polkit-kde-agent = {
+      description = "kde polkit authentication agent";
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.kdePackages.polkit-kde-agent-1}/libexec/polkit-kde-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+      install = {
+        WantedBy = [ "graphical-session.target" ];
       };
     };
   };
 }
-
