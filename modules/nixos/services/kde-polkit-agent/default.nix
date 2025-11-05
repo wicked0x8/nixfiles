9b7566f3bd0d196fa23b6e89de52479bfe358ed4
dubbber <scorpionolegovich@gmail.com>
2025-11-05 20:33:19 +0500

    added cliphist, kdepolkitagent, and env vars

--- modules/nixos/services/kde-polkit-agent/default.nix
+++ modules/nixos/services/kde-polkit-agent/default.nix
@@ -21,7 +21,7 @@ in
         RestartSec = 1;
         TimeoutStopSec = 10;
       };
-      WantedBy = [ "graphical-session.target" ];
+      wantedBy = [ "graphical-session.target" ];
     };
   };
 }
