f876e095efd9501f6b7ad42dde9c03c711083982
dubbber <scorpionolegovich@gmail.com>
2025-11-09 16:26:07 +0500

    added zapret and hydralauncher

--- modules/nixos/services/zapret/default.nix
+++ modules/nixos/services/zapret/default.nix
@@ -13,6 +13,27 @@ in
     environment.systemPackages = with pkgs; [ zapret ];
     services.zapret = {
       enable = true;
+      params = ''
+        [
+	  --filter-tcp=443
+	  --dpi-desync=fake,fakeddisorder
+	  --dpi-desync-split-pos=10,midsld
+	  --dpi-desync-fake-tls=/opt/zapret/files/fake/tls_clienthello_www_google_com.bin
+	  --dpi-desync-fake-tls-mod=rnd,dupsid,sni=fonts.google.com
+	  --dpi-desync-fake-tls=0x0F0F0F0F
+	  --dpi-desync-fake-tls-mod=none
+	  --dpi-desync-fakedsplit-pattern=/opt/zapret/files/fake/tls_clienthello_vk_com.bin
+	  --dpi-desync-split-seqovl=336
+	  --dpi-desync-split-seqovl-pattern=/opt/zapret/files/fake/tls_clienthello_gosuslugi_ru.bin
+	  --dpi-desync-fooling=badseq,badsum
+	  --dpi-desync-badseq-increment=0
+	  --new
+	  --filter-udp=443
+	  --dpi-desync=fake
+	  --dpi-desync-repeats=4 
+	  --dpi-desync-fake-quic=/opt/zapret/files/fake/quic_initial_www_google_com.bin
+	]
+      '';
     };
   };
 }
