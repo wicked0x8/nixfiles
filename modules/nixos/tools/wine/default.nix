80a77677e88f21e44486fa7a387c35984bf186b7
dubbber <scorpionolegovich@gmail.com>
2025-11-11 21:15:43 +0500

    tryna play dispatch, installing .net and wine

--- modules/nixos/tools/wine/default.nix
+++ modules/nixos/tools/wine/default.nix
@@ -16,6 +16,6 @@ in
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [ wineWowPackages.staging winemono ];
+    environment.systemPackages = with pkgs; [ wineWowPackages.staging wineWowPackages.waylandFull winetricks ];
   };
 }
