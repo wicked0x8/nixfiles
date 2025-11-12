ebea49a9145a2fd83c35993e7a0376117619550f
dubbber <scorpionolegovich@gmail.com>
2025-11-12 17:19:47 +0500

    tryna play dispatch, installing .net and wine

--- modules/nixos/system/amdcpu-tweaks/default.nix
+++ modules/nixos/system/amdcpu-tweaks/default.nix
@@ -14,6 +14,9 @@ in
     services.system76-scheduler.settings.cfsProfiles.enable = true; # Better scheduling for CPU cycles - thanks System76!!!
     services.power-profiles-daemon.enable = true;
     environment.systemPackages = with pkgs; [ vulkan-tools ];
+    hardware.opengl.enable = true;
+    hardware.opengl.driSupport = true;
+    hardware.opengl.driSupport32Bit = true;
   };
 }
 
