b8403949a61e3766113fe7728054a5b6c770ecaf
dubbber <scorpionolegovich@gmail.com>
2025-11-12 17:37:30 +0500

    tryna play dispatch, installing .net and wine

--- modules/nixos/system/amdcpu-tweaks/default.nix
+++ modules/nixos/system/amdcpu-tweaks/default.nix
@@ -20,7 +20,7 @@ in
       enable32Bit = true;
     };
 
-    environment.variables = { VK_ICD_FILENAMES = "run/opengl-driver/share/vulkan/icd.d/amd_icd64.json" };
+    environment.variables = { VK_ICD_FILENAMES = "run/opengl-driver/share/vulkan/icd.d/amd_icd64.json"; };
 }
 
 
