1c6c969b439c09cf8943dd90b1d073e0cb9964ca
dubbber <scorpionolegovich@gmail.com>
2025-11-15 20:22:07 +0500

    i finished the game, absolute cinema, study time

--- modules/home/desktop/mango/default.nix
+++ modules/home/desktop/mango/default.nix
@@ -99,8 +99,8 @@ in
           inhibit_regardless_of_visibility=0
           sloppyfocus=1
           warpcursor=1
-          focus_cross_monitor=0
-          focus_cross_tag=0
+          focus_cross_monitor=
+          focus_cross_tag=
           enable_floating_snap=0
           snap_distance=30
           cursor_size=24
