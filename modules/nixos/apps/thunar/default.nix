c39617b4926ea64c003732e16089da9c3264ec4c
dubbber <scorpionolegovich@gmail.com>
2025-11-17 16:47:56 +0500

    installed intelij idea and thunar

--- modules/nixos/apps/thunar/default.nix
+++ modules/nixos/apps/thunar/default.nix
@@ -1,14 +1,14 @@
 { lib, config, pkgs, ... }:
 let
   inherit (lib) mkEnableOption mkIf;
-  cfg = config.mine.apps.firefox;
+  cfg = config.mine.apps.thunar;
 in
 {
-  options.mine.apps.firefox = {
-    enable = mkEnableOption "install firefox";
+  options.mine.apps.thunar = {
+    enable = mkEnableOption "install thunar";
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [ firefox ];
+    environment.systemPackages = with pkgs; [ thunar ];
   };
 }
