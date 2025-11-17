077439f0e2356ada1d25c31a4430ef4dfe925296
dubbber <scorpionolegovich@gmail.com>
2025-11-17 16:43:51 +0500

    installed intelij idea

---
+++ modules/nixos/apps/intellij-idea/default.nix
@@ -0,0 +1,14 @@
+{ lib, config, pkgs, ... }:
+let
+  inherit (lib) mkEnableOption mkIf;
+  cfg = config.mine.apps.intellij-idea;
+in
+{
+  options.mine.apps.intellij-idea = {
+    enable = mkEnableOption "install intellij idea community";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [ intellij-idea-community ];
+  };
+}
