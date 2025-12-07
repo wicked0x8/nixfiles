edbf5643fb7713789d3afc7e8e4695f6fe38f9ab
dubbber <scorpionolegovich@gmail.com>
2025-12-07 22:45:16 +0500

    tried zoxide, impressive tool

---
+++ modules/nixos/tools/claude-code/default.nix
@@ -0,0 +1,21 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+let
+
+  inherit (lib) mkEnableOption mkIf;
+  cfg = config.mine.tools.claude-code;
+
+in
+{
+  options.mine.tools.claude-code = {
+    enable = mkEnableOption "enable claude code";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [ claude-code ];
+  };
+}
