b15c664b9d52071df634ed21076780405f2f19e9
dubbber <scorpionolegovich@gmail.com>
2025-12-31 13:41:44 +0500

    trying out helix + minor changes to mangowc

--- modules/home/desktop/mango/default.nix
+++ modules/home/desktop/mango/default.nix
@@ -272,7 +272,7 @@ in
           # Mouse Button Bindings
           # NONE mode key only work in ov mode
           mousebind=SUPER,btn_left,moveresize,curmove
-          mousebind=NONE,btn_middle,togglemaximizescreen,0
+          #mousebind=NONE,btn_middle,togglemaximizescreen,0
           mousebind=SUPER,btn_right,moveresize,curresize
           mousebind=NONE,btn_left,toggleoverview,-1
           mousebind=NONE,btn_right,killclient,0
